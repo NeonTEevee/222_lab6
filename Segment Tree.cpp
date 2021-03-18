629ba59761a3dd23d8aba556887176d358ccf589
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-18 16:23:02 +0700

    Finished

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -1,15 +1,14 @@
 #include <stdio.h>
 int main ()
 {
-	int x, y, N, Q, max = 0;
+	int x, y, N, Q, tempt, max = 0;
 	scanf("%d %d", &N, &Q);
 	char command[Q];
 	int i[N], value[Q][Q];
 	for(x=0; x<Q; x++)
 	{
 		scanf(" %c", &command[x]);
-		scanf("%d %d", &value[x+1][0], &value[0][x+1]);
-		printf("Command #%d = %c\nValue[%d][0] = %d\nValue[0][%d] = %d\n", x+1, command[x], x+1, value[x+1][0], x+1, value[0][x+1]);
+		scanf("%d %d", &value[x+1][0], &value[0][x]);
 	}
 	
 	for(x=0; x<=N; x++)
@@ -19,12 +18,13 @@ int main ()
 	{
 		if(command[x] == 'U')
 		{
-			i[value[x+1][0]] = value[0][x+1];
-			printf("i[%d] = %d\n", value[x+1][0], value[0][x+1]);
+			tempt = value[x+1][0];
+			i[tempt] = value[0][x];
 		}
 		if(command[x] == 'P')
 		{
-			for(y=value[x+1][0]; y<=value[0][x+1]; y++)
+			max = 0;
+			for(y=value[x+1][0]; y<=value[0][x]; y++)
 			{
 				if(max < i[y])
 					max = i[y];
